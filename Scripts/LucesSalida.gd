238ff93e06de498a92af27460668ed0d30251497
AdriSabadell <e.amartinezl@ies-sabadell.cat>
2024-05-07 21:25:39 +0200

    Pequeños Cambios Luces

--- Scripts/LucesSalida.gd
+++ Scripts/LucesSalida.gd
@@ -12,12 +12,14 @@ func _ready():
 func _process(delta):
 	if empieza_cuenta == true:
 		tiempo_salida += get_process_delta_time()
-	print(tiempo_salida)
+	#print(tiempo_salida)
 	if tiempo_salida > 2:
 		$RedLight.visible = true
 	if tiempo_salida > 4:
 		empieza_cuenta = false
 		$RedLight.visible = false
+		
 func _on_body_entered(body):
 	if body.is_in_group("Player") and tiempo_salida < 4:
 		body.penalizacion()
+		body.empiezaEtapa()
